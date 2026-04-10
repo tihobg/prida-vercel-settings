debe9d8ce41b21bdd812e75fdd6c4fc4b6a0dabf
Tihomir Tiankov <tiho_bg@abv.bg>
2026-04-10 11:34:21 +0300

    Prida New Vercel Settings

--- pridastartpr/pridastartpr/settings.py
+++ pridastartpr/pridastartpr/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-7y3a18g=&prmr!kd&pn-3d=1@gdd(y(tb(n8xg(_tts4%kkgva
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = True
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
