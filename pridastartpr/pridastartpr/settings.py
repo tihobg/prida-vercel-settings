e245ea879b48ea409a32a629560348d9b2390f68
Tihomir Tiankov <tiho_bg@abv.bg>
2026-04-26 23:22:41 +0300

    Test deployment

--- pridastartpr/pridastartpr/settings.py
+++ pridastartpr/pridastartpr/settings.py
@@ -12,12 +12,17 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 import dj_database_url
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / '.env')  # Use explicit path
+
+print("DEBUG - DATABASE_URL:", os.environ.get('DATABASE_URL'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -80,29 +85,18 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'pridastartpr.wsgi.application'
-
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=60,
+        ssl_require=True
+    )
+}
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DATABASE'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('POSTGRES_HOST'),
-        'PORT': '5432',
-        'OPTIONS': {
-            'sslmode': 'require',
-        },
-        'CONN_MAX_AGE': 60,
-    }
-}
+
 
 # DATABASES["default"] = dj_database_url.parse(config('DATABASE_URL'))
 
